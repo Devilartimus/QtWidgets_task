f876c4a774eb231143196baf679ccef7a9d736b6
Artem Tikhonov <artemtihonov@list.ru>
2024-09-09 21:24:02 +0700

    Deleting excess

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Touchstone_Task_FILE C:/Users/user/Desktop/test/Touchstone_Task/build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/Touchstone_Task.exe)
-set(__QT_DEPLOY_TARGET_Touchstone_Task_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Touchstone_Task_RUNTIME_DLLS E:/qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;E:/qt/6.7.2/mingw_64/bin/Qt6Gui.dll;E:/qt/6.7.2/mingw_64/bin/Qt6Core.dll)
