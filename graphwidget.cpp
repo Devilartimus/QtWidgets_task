8261a30e3f9e276bad62eb4d11383058d9bbbce1
Artem Tikhonov <artemtihonov@list.ru>
2024-09-09 18:24:24 +0700

    Added title for axes

--- graphwidget.cpp
+++ graphwidget.cpp
@@ -20,13 +20,21 @@ void GraphWidget::paintEvent(QPaintEvent *event)
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
-    painter.drawLine(10, height() - 10, width() - 10, height() - 10);
-    painter.drawLine(10, 10, 10, height() - 10);
+    painter.drawLine(40, height() - 40, width() - 20, height() - 40);
+    painter.drawLine(40, 20, 40, height() - 40);
 
     if(!dataPoints.isEmpty())
     {
         drawGraph(painter);
     }
+
+    painter.setFont(QFont("Arial", 10));
+    painter.drawText(width() / 2 - 40, height() - 10, "Frequency");
+    painter.save();
+
+    painter.rotate(-90);
+    painter.drawText(-height() / 2 - 40, 20, "S11LogMag");
+    painter.restore();
 }
 
 void GraphWidget::drawGraph(QPainter &painter)
@@ -47,10 +55,10 @@ void GraphWidget::drawGraph(QPainter &painter)
 
     for (int i = 1; i < dataPoints.size(); i++)
     {
-        QPoint p1 = QPoint(10 + (dataPoints[i - 1].x() - minFreq) / (maxFreq - minFreq) * (width() - 20),
-                            height() - 10 - (dataPoints[i - 1].y() - minLogMag) / (maxLogMag - minLogMag) * (height() - 20));
-        QPoint p2 = QPoint(10 + (dataPoints[i].x() - minFreq) / (maxFreq - minFreq) * (width() - 20),
-                           height() - 10 - (dataPoints[i].y() - minLogMag) / (maxLogMag - minLogMag) * (height() - 20));
+        QPoint p1 = QPoint(40 + (dataPoints[i - 1].x() - minFreq) / (maxFreq - minFreq) * (width() - 60),
+                           height() - 40 - (dataPoints[i - 1].y() - minLogMag) / (maxLogMag - minLogMag) * (height() - 60));
+        QPoint p2 = QPoint(40 + (dataPoints[i].x() - minFreq) / (maxFreq - minFreq) * (width() - 60),
+                           height() - 40 - (dataPoints[i].y() - minLogMag) / (maxLogMag - minLogMag) * (height() - 60));
         painter.drawLine(p1, p2);
     }
 }
